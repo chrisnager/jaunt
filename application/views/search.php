140fe29ee3cdab750bb747c697d58c7ff754c224
Chris Nager <cnager@gmail.com>
2014-05-03 19:02:11 -0400

    Update header bar css

--- application/views/search.php
+++ application/views/search.php
@@ -1,1 +1,10 @@
-test
+<header class="bar bar-nav">
+    <h1 class="title">Jaunt</h1>
+</header>
+<div class="content">
+    <div class="search-box">
+        <input type="search">
+    </div>
+    <div class="content-padded">
+    </div>
+</div>
