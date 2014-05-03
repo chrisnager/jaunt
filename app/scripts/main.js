6cea6704a43bc09fc27716417df27cd5546762aa
mmaatthheeww <mmaatthheeww@gmail.com>
2014-05-03 16:02:20 -0400

    using that yomang to scaffold

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global JauntApp, $*/
+
+
+window.JauntApp = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    JauntApp.init();
+});
