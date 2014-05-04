6688d59119c11425028300aefb8ef27584ac3fe2
clintonhalpin <clintonhalpin@gmail.com>
2014-05-03 22:03:09 -0400

    Change background and fix links

--- application/views/index.php
+++ application/views/index.php
@@ -8,28 +8,18 @@
 
 <div id="add" class="modal">
   <header class="bar bar-nav">
-    <h1 class="title">Name Your Jaunt</h1>
+    <h1 class="title">New Jaunt</h1>
+    <a class="pull-right" href="#search"><span class="icon icon-right"></span></a>
   </header>
   <div class="content content--nobg">
    	<form class="input-group">
 	  <div class="input-row">
-	    <label>Name</label>
-	    <input type="text" placeholder="">
+	    <input type="text" placeholder="Name your place...">
 	  </div>
 	  <div class="input-row">
-	    <label>Description</label>
-	    <input type="email" placeholder="">
+	    <input type="email" placeholder="Add a Description...">
 	  </div>
 	</form>
-	<a data-transition="slide-in" href="#search" class="btn btn-block">Next</a>
-  </div>
-   <div class="content content--nobg">
-   	<form class="input-group">
-	  <div class="input-row">
-	    <input type="text" placeholder="Search">
-	  </div>
-	</form>
-	
   </div>
 </div>
 
@@ -43,7 +33,7 @@
 	    <input type="text" placeholder="Search">
 	  </div>
 	</form>
-	<a data-transition="slide-in" href="/jaunt" class="btn btn-block">Add</a>
+	<a data-transition="slide-in" href="/jaunts" class="btn btn-block">Add</a>
   </div>
 </div>
 
--- application/views/search.php
+++ application/views/search.php
@@ -1,10 +1,10 @@
 <header class="bar bar-nav jaunt-bar">
-      <a class="pull-left" href="/add">Cancel</a>
+      <a class="pull-left" href="/add"><span class="icon icon-left"></span></a>
       <h1 class="title">Add a Place</h1>
 </header>
 <div class="content">
 <form>
-    <input type="text" placeholder="Search">
+    <input type="text" placeholder="Search for a location">
 </form>
 </div>
 
