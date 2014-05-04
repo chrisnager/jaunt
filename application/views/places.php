61504d03bd4eb7e56f2c38664312b82327e940b3
Chris Nager <cnager@gmail.com>
2014-05-04 00:53:28 -0400

    Update places

--- application/views/places.php
+++ application/views/places.php
@@ -2,15 +2,19 @@
   <h1 class="title">jaunt</h1>
 </header>
 <div class="content">
-  <div class="content-padded">
     <?php foreach($place as $row): ?>
-      <h4><?= $row->name ?></h4>
-      <small><?= $response['result']['formatted_address'] ?></small>
-      <?php if (isset($response['result']['website'])) { ?>
-        <a href="<?= $response['result']['website'] ?>" target="_blank"><?= $response['result']['website'] ?></a>
-      <?php } ?>
-      <p><?= $row->note ?></p>
+      <div class="place">
+          <div class="place__image">
+            <img src="http://24.media.tumblr.com/20a9c501846f50c1271210639987000f/tumblr_n4vje69pJm1st5lhmo1_1280.jpg" alt="">
+          </div>
+          <div class="place__info content-padded">
+              <h3 class="place__title"><?= $row->name ?></h3>
+              <p class="place__address"><small><?= $response['result']['formatted_address'] ?></small></p>
+              <?php if (isset($response['result']['website'])) { ?>
+                <p class="place__site"><a href="<?= $response['result']['website'] ?>" target="_blank"><?= $response['result']['website'] ?></a></p>
+              <?php } ?>
+              <p class="place__note"><?= $row->note ?></p>
+          </div>
+      </div>
     <?php endforeach; ?>
-  </div>
 </div>
-
