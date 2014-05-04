232a58621965ec99cc83eb6fe47d1829167c7f8c
mmaatthheeww <mmaatthheeww@gmail.com>
2014-05-04 03:10:52 -0400

    fixing search default page so it doesnt fuck up

--- application/views/search.php
+++ application/views/search.php
@@ -6,17 +6,19 @@
 <form>
     <input type="text" placeholder="Search for a location">
 </form>
-  <ul>
-  <?php foreach ($response['results'] as $result): ?> 
-    <li>
-      <?php if(isset($result['photos'])) { ?> 
-      <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=640&photoreference=<?= $result['photos'][0]['photo_reference'] ?>&sensor=true&key=AIzaSyCsjrmFW0bjXRJfRaLZdukRmkxTKUxzT3I">
-      <?php } ?>
-      <h4><?= $result['name'] ?></h4>
-      <small><?= $result['formatted_address'] ?></small>
-    </li>
-  <?php endforeach; ?>
-  </ul>
+  <?php if(isset($response)) { ?>
+    <ul>
+      <?php foreach ($response['results'] as $result): ?> 
+        <li>
+          <?php if(isset($result['photos'])) { ?> 
+            <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=640&photoreference=<?= $result['photos'][0]['photo_reference'] ?>&sensor=true&key=AIzaSyCsjrmFW0bjXRJfRaLZdukRmkxTKUxzT3I">
+          <?php } ?>
+          <h4><?= $result['name'] ?></h4>
+          <small><?= $result['formatted_address'] ?></small>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  <?php } ?>
 </div>
 
 
