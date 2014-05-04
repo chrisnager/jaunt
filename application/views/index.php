6f2da6b45a0b40c6c0c8f0408faf18874fdd5dab
Chris Nager <cnager@gmail.com>
2014-05-03 21:36:51 -0400

    Update create to add

--- application/views/index.php
+++ application/views/index.php
@@ -2,7 +2,7 @@
     <h1 class="title">Jaunt</h1>
 </header>
 <div class="content">
-    <a class="btn  btn-white  btn-padded  btn-block  btn-outlined" href="/create">Create a Jaunt</a>
+    <a class="btn  btn-white  btn-padded  btn-block  btn-outlined" href="/add">Add a Jaunt</a>
     <a class="btn  btn-white  btn-padded  btn-block  btn-outlined" href="/find">Find a Jaunt</a>
 </div>
 
